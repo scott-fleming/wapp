b087dbe6ee8c527bc14a405f66ecb005a83fff96
Scott Fleming <sdflem@gmail.com>
2018-09-21 21:15:11 +0000

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+MultipleChoiceQuestion.create!(question: 'What method do we use to create a model record?', answer: 'create', distractor_1: 'update', distractor_2: 'delete', distractor_3: 'all', distractor_4: 'find')
+
+MultipleChoiceQuestion.create!(question: 'What does the C in MVC stand for?', answer: 'Controller', distractor_1: 'Const', distractor_2: 'Code', distractor_3: 'Create', distractor_4: 'Comm')
+
+MultipleChoiceQuestion.create!(question: 'What method do we use to delete a model record', answer: 'delete', distractor_1: 'update', distractor_2: 'create', distractor_3: 'all', distractor_4: 'find')
