925976775cff36184d821b44b6aa7b0838548349
Scott Fleming <sdflem@gmail.com>
2018-10-03 10:51:38 +0000

    Updated home page to link to random question

--- app/controllers/multiple_choice_questions_controller.rb
+++ app/controllers/multiple_choice_questions_controller.rb
@@ -1,20 +1,26 @@
 class MultipleChoiceQuestionsController < ApplicationController
   def ask
-    @multiple_choice_question = MultipleChoiceQuestion.find(params[:id])
-    # TODO: Error handling
+    begin
+      @multiple_choice_question = MultipleChoiceQuestion.find(params[:id])
+    rescue
+      redirect_to home_url, alert: "Error: Question not found."
+    end
     # render 'multiple_choice_questions/ask.html.erb'
   end
 
   def check_answer
-    @multiple_choice_question = MultipleChoiceQuestion.find(params[:id])
-    # TODO: Error handling
+    begin
+      @multiple_choice_question = MultipleChoiceQuestion.find(params[:id])
+    rescue
+      redirect_to home_url, alert: "Error: Question not found."
+    end
 
     if params[:guess] && params[:guess] == '0'
       redirect_to home_url, notice: "Right! #{@multiple_choice_question.answer} was the correct answer."
     elsif params[:guess] && ['1', '2', '3', '4'].include?(params[:guess])
       redirect_to home_url, alert: "Wrong! Model was the correct answer."
     else
-      # TODO: Error handling
+      redirect_to home_url, alert: "Error: Invalid answer."
     end
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def home
+    question_id = MultipleChoiceQuestion.all.pluck(:id).sample
+    @multiple_choice_question = MultipleChoiceQuestion.find(question_id)
     # render 'home.html.erb'
   end
 
